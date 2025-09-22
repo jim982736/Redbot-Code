4fb63050e04dc60b4fe0ad4eb9b70e5f4180cd5c
Ben <bhd3@illinois.edu>
2025-09-22 10:16:28 -0500

    added main ino files

---
+++ Redbot-Uno-Control.ino
@@ -0,0 +1,346 @@
+#include <Servo.h>
+#include <math.h>
+
+#define AVERAGE(a, b) ((a + b) / 2)
+
+// Left Motor
+#define AIN1 13
+#define AIN2 12
+#define APWR 11
+
+#define L_ENC_A 3   // White
+#define L_ENC_B A0  // Green
+
+// Right Motor
+#define BIN1 8
+#define BIN2 9
+#define BPWR 10
+
+#define R_ENC_A 2   // White
+#define R_ENC_B A1  // Green
+// Blue -> GND
+// Yellow -> 5V
+
+const int servoPin = 6;
+const int buttonPin = 7;
+
+volatile int rightEncoderValue = 0;
+volatile int leftEncoderValue = 0;
+
+volatile int upCnt = 0;
+volatile int dwnCnt = 0;
+
+// defining as bytes since these are read directly from ports
+byte rStateA = 0;
+byte rStateB = 0;
+byte lStateA = 0;
+byte lStateB = 0;
+
+const int countsPerRev = 288;  // 48:1 gear ratio, 6 ticks per revolution on encoder
+const double wheelDia = 6.6;   // measured wheel to be about 66mm in diameter
+const double wheelCirc = PI * wheelDia;
+const double wheelDist = 5.0;  // approx distance from the center of the wheels to the center of the robot
+
+// drive distance cm at power adjusting the motors every rate ms
+void driveStraight(double distance, int power, int rate = 50);
+
+int lCnt = 0;
+int rCnt = 0;
+
+int logl[250] = {0};
+int logr[250] = {0};
+
+void setup() {
+  logl[499] = 1000000;
+  
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(APWR, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(BPWR, OUTPUT);
+
+  pinMode(L_ENC_A, INPUT);
+  pinMode(L_ENC_B, INPUT);
+  pinMode(R_ENC_A, INPUT);
+  pinMode(R_ENC_B, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(L_ENC_A), leftEncoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(R_ENC_A), rightEncoderISR, CHANGE);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(9600);
+  //clearEncoders();
+}
+
+void loop() {
+  /*int left = 0, right = 0;
+  readEncoders(&left, &right);
+  Serial.print(left);
+  Serial.print('\t');
+  Serial.println(right);
+  delay(50);*/
+
+  if (digitalRead(buttonPin) == LOW) {
+    Serial.println("Go");
+    //driveStraight(30.0, 200);
+
+    clearEncoders();
+    reset();
+
+    int lCount = 0;
+    int rCount = 0;
+
+    int* leftLog = logl;
+    int* rightLog = logr;
+
+    int errorCount = 0;
+
+    driveForward(200);
+    readEncoders(&lCount, &rCount);
+    while (AVERAGE(lCount, rCount) < 1000) {
+      readEncoders(&lCount, &rCount);
+
+      if (*leftLog != 1000000) {
+        *leftLog = lCount;
+        *rightLog = rCount;
+        leftLog++;
+        rightLog++;
+      }
+
+      if (lCount < 0 || rCount < 0) {
+        errorCount++;
+      }
+
+      delay(10);
+    }
+    brake();
+    Serial.println("done");
+
+    // print logs out
+    leftLog = logl;
+    rightLog = logr;
+
+    Serial.println("Left   Right   Target");
+    for (int i = 0; i < 250; i++) {
+      Serial.print(*leftLog++);
+      Serial.print("\t");
+      Serial.print(*rightLog++);
+      Serial.print("\t");
+      Serial.println(500);
+    }
+
+    noInterrupts();
+    int up = upCnt;
+    int down = dwnCnt;
+    interrupts();
+
+    Serial.print("Error Count: ");
+    Serial.println(errorCount);
+    Serial.print("up: ");
+    Serial.print(up);
+    Serial.print("\tdown: ");
+    Serial.println(down);
+  } 
+
+  /*if (digitalRead(buttonPin) == LOW) {
+    readEncoders(&lCnt, &rCnt);
+    Serial.print(lCnt);
+    Serial.print("\t");
+    Serial.println(rCnt);
+  } else {
+    clearEncoders();
+  }
+  delay(50);*/ 
+}
+
+void reset() {
+  for (int i = 0; i < 250; i++) {
+    logl[i] = logr[i] = 0;
+  }
+
+  noInterrupts();
+  upCnt = 0;
+  dwnCnt = 0;
+  interrupts();
+}
+
+// Drive Control
+void driveStraight(double distance, int power, int rate) {
+  delay(100);
+
+  int lCount = 0;
+  int rCount = 0;
+  int targetCount;
+  double revCount;
+  double overshoot = 0.0;  // measure overshoot
+
+  int lPrevCount = 0, rPrevCount = 0;  // variables to track left and right encoder counts
+  int lDiff, rDiff;                    // difference between current and previous encoder counts
+
+  int leftPower = power, rightPower = power;  // variables for adjusting motor power
+
+  int offset = 5;
+  double error;
+
+  revCount = (distance - overshoot) / wheelCirc;  // target number of rotations, subtracting overshoot
+  targetCount = revCount * countsPerRev;
+
+  // print header
+  Serial.print("driveStraight target: ");
+  Serial.print(targetCount);
+  Serial.println(" revolutions");
+  Serial.println("Left   Right   Target   Error");
+
+  clearEncoders();
+  delay(200);  // short delay before starting motors
+
+  driveForward(power);
+
+  long cnt = 0;
+  int time = 0;
+  int startTime = millis();
+
+  while (AVERAGE(lCount, rCount) < targetCount) {
+    // Wait until reaching target while adjusting encoders to keep line straight
+
+    if (time >= 50) {
+      time = 0;
+      startTime = millis();
+
+      readEncoders(&lCount, &rCount);
+
+      Serial.print(lCount);
+      Serial.print("\t");
+      Serial.print(rCount);
+      Serial.print("\t");
+      Serial.print(targetCount);
+      Serial.print("\t");
+
+      // account for some occasional sporadic readings at the beginning of driveStraight
+      /*if (cnt < 3 && (lCount < 0 || rCount < 0)) {
+        Serial.println("\n---reset---\n");
+        clearEncoders();
+        lCount = rCount = 0;
+      }
+      cnt++;*/
+
+      // calculate difference in encoder counts from previous cycle
+      lDiff = (lCount - lPrevCount);
+      rDiff = (rCount - rPrevCount);
+      error = (double)abs((lDiff - rDiff)) / 5.0;  // determine error between encoder counts and normalize it by 5
+      Serial.println(error);
+
+      // store the current count as the "previous" count for the next cycle.
+      lPrevCount = lCount;
+      rPrevCount = rCount;
+
+      // skip update if error seems to large, likely innacurate
+      /*if (error > 3.0) {
+        delay(rate);
+        continue;
+      }*/
+
+      // if left is faster than the right, slow down the left / speed up right
+      if (lDiff > rDiff) {
+        leftPower -= (int)(offset * error);
+        rightPower += (int)(offset * error);
+      }
+      // if right is faster than the left, speed up the left / slow down right
+      else if (lDiff < rDiff) {
+        leftPower += (int)(offset * error);
+        rightPower -= (int)(offset * error);
+      }
+
+      // update motor power
+      driveLeft(leftPower);
+      driveRight(rightPower);
+    }
+    time = millis() - startTime;
+  }
+  // stop after exiting loop
+  brake();
+}
+
+// Motor Control Functions
+void driveRight(int power) {
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, HIGH);
+  analogWrite(BPWR, power);
+}
+
+void driveLeft(int power) {
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(APWR, power);
+}
+
+void driveForward(int power) {
+  driveLeft(power);
+  driveRight(power);
+}
+
+void coast() {
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, LOW);
+}
+
+void brake() {
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, HIGH);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, HIGH);
+  analogWrite(APWR, 0);
+  analogWrite(BPWR, 0);
+}
+
+// Encoder Control
+void clearEncoders() {
+  noInterrupts();
+  rightEncoderValue = 0;
+  leftEncoderValue = 0;
+  interrupts();
+}
+
+void readEncoders(int* left, int* right) {
+  noInterrupts();
+  *left = leftEncoderValue;
+  *right = rightEncoderValue;
+  interrupts();
+}
+
+// Encoder ISRs
+// Reading directly from ports to speed up runtime
+void leftEncoderISR() {
+  lStateA = (PIND & B00001000) / 8;  // pin 3 on port D
+  lStateB = PINC & B00000001;        // pin A0 on port C
+  // dividing to make sure states are between 0 and 1
+
+  /*lStateA = digitalRead(L_ENC_A);
+  lStateB = digitalRead(L_ENC_B);*/
+
+  if (lStateA == lStateB) {
+    leftEncoderValue--;
+  } else {
+    leftEncoderValue++;
+  }
+}
+
+void rightEncoderISR() {
+  rStateA = (PIND & B00000100) / 4;  // pin 2 on port D
+  rStateB = (PINC & B00000010) / 2;  // pin A1 on port C
+  // dividing to make sure states are between 0 and 1
+
+  /*rStateA = digitalRead(R_ENC_A);
+  rStateB = digitalRead(R_ENC_B);*/
+
+  if (rStateA == rStateB) {
+    upCnt++;
+    rightEncoderValue++;
+  } else {
+    dwnCnt++;
+    rightEncoderValue--;
+  }
+}
---
+++ redbot-control-main.ino
@@ -0,0 +1,333 @@
+// libraries for gyro
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_L3GD20_U.h>
+
+#include <RedBot.h>
+#include <Servo.h>
+#define AVERAGE(a, b) ((a + b) / 2)
+#define ABS(n) ((n < 0) ? n * -1 : n)
+
+RedBotMotors motors;
+
+// assign unique ID to gryo, seems like it needs to be named "gyro"
+Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
+
+RedBotEncoder encoder = RedBotEncoder(A2, 10);
+int buttonPin = 12;
+int countsPerRev = 192;  // 48:1 gearing, 4 ticks per revolution
+
+float wheelDiam = 6.7;             // 2.6 diam = 65mm, measured wheel to be closer to 67mm
+float wheelCirc = PI * wheelDiam;  // Redbot wheel circumference = pi*D
+float wheelDist = 5.0;             // approx. dist from center of redbot to center of wheels
+
+const int servoPin = 9;
+Servo marker;
+
+int PIDController(double error, int propGain = 0, int integralGain = 0, int derGain = 0);
+
+struct PID {
+  double integState;
+  double integMax, integMin;
+  double derState;
+  int propGain, derGain, integGain;
+};
+
+// update values in PID based on the error
+int updatePID(PID* pid, double error, double dist) {
+  int prop, integ, der;
+
+  // calculate proportional term
+  prop = pid->propGain * error;
+
+  // calculate integral state with limiting
+  pid->integState += error;
+  if (pid->integState > pid->integMax)
+    pid->integState = pid->integMax;
+  else if (pid->integState < pid->integMin)
+    pid->integState = pid->integMin;
+
+  integ = pid->integGain * pid->integState;
+
+  // calculate derivative term
+  der = pid->derGain * (pid->derState - dist);
+  pid->derState = dist;
+
+  // print values to Serial port for tuning
+  /*Serial.print(pid->integState);
+  Serial.print('\t');
+  Serial.print(integ);
+  Serial.print('\t');
+  Serial.print(prop);
+  Serial.print('\t');
+  Serial.print(der);
+  Serial.print('\t');*/
+
+  return prop + integ + der;
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  marker.write(0);
+  marker.attach(servoPin);
+  Serial.begin(9600);
+
+  gyro.enableAutoRange(true);
+
+  if (!gyro.begin()) {
+    Serial.println("--gyro not detected, check wiring--");
+    while (true)
+      ;
+  }
+}
+
+void loop(void) {
+  // set the power for left & right motors on button press
+  if (digitalRead(buttonPin) == LOW) {
+    /*delay(200);
+    marker.write(15);
+    delay(2000);
+    marker.write(28);
+    delay(200);*/
+
+    // demo
+    turnToAngleGyro(60.0);
+    turnToAngleGyro(-150.0);
+    driveStraight(40.0, 100);
+    turnToAngleGyro(120.0);
+    driveStraight(20.0, 100);
+    turnToAngleGyro(120.0);
+    driveStraight(40.0, 100);
+    turnToAngleGyro(-120.0);
+    driveStraight(20.0, 100);
+    turnToAngleGyro(-30.0);
+
+    // 180 turn
+    /*driveStraight(20, 100);
+    turnToAngle(180, 100);
+    driveStraight(20, 100);
+    turnToAngle(180, 100);*/
+
+    // Single Line
+    /*driveStraight(50, 100);
+    marker.write(15);*/
+
+    // multiple turns
+    /*turnToAngleGyro(90.0);
+    turnToAngleGyro(-120.0);
+    turnToAngleGyro(420.0);
+    turnToAngleGyro(-360.0)*/
+
+    // triangle
+    /*driveStraight(40, 100);
+    turnToAngleGyro(120.0);
+    driveStraight(40, 100);
+    turnToAngleGyro(120.0);
+    driveStraight(40, 100);
+    turnToAngleGyro(120.0);*/
+  }
+}
+
+// counter clockwise for positive angles and clockwise for negative ones
+void turnToAngleGyro(double angle) {
+  marker.write(15);
+
+  double target = angle * (PI / 180.0);
+  double dist = 0.0;
+
+  Serial.print("Target: ");
+  Serial.println(angle);
+
+  // variables for tracking distance travelled
+  int prevTime = millis();
+  int currTime = 0;
+  double timeDiff = 0;
+
+  // control variables
+  double error = target;
+  int drive = 0;
+  int ceiling = 100;  // highest motor power with ability to track movement
+  int finishCount = 0;
+
+  // initialize PID controller for turning, adjust gains to tune controller
+  PID pid;
+  pid.propGain = 625;
+  pid.integGain = 31;
+  pid.derGain = 1250;
+  pid.derState = 0.0;
+  pid.integMax = 20.0;
+  pid.integMin = -20.0;
+  pid.integState = 0.0;
+
+  delay(200);
+  motors.pivot(ceiling);
+
+  while (finishCount < 10) {
+    sensors_event_t event;
+    gyro.getEvent(&event);
+
+    currTime = millis();
+    timeDiff = currTime - prevTime;
+    prevTime = currTime;
+    timeDiff /= 1000;
+    dist += timeDiff * event.gyro.z;
+
+    //PI controller to determine motor power
+    error = target - dist;
+    drive = updatePID(&pid, error, dist);
+
+    // give drive a ceiling and floor to ensure enough torque
+    if (drive > ceiling) {
+      drive = ceiling;
+    } else if (drive < (-1 * ceiling)) {
+      drive = -1 * ceiling;
+    }
+
+    if (ABS(error) < 0.01) {
+      finishCount++;
+    }
+
+    // pivot with new motor power and wait 25ms for motors to respond
+    motors.pivot(drive);
+
+    /*Serial.print(error);
+    Serial.print('\t');
+    Serial.println(drive);*/
+
+    delay(25);
+  }
+  motors.brake();
+}
+
+// Unused turning function based purely on encoders
+void turnToAngle(double angle, int motorPower) {
+  double overshoot = 20;                                                    // measured overshoot
+  double arcLength = ((angle - overshoot) / 360.0) * (wheelDist * 2 * PI);  // distance traveled by each wheel
+  double numRev = arcLength / wheelCirc;
+
+  long targetCount = numRev * countsPerRev;
+  long lCount = 0;
+  long rCount = 0;
+  int power = ((angle > 0) ? motorPower : (motorPower * -1));
+
+  encoder.clearEnc(BOTH);
+  marker.write(15);
+  delay(200);
+  motors.pivot(power);
+
+  Serial.print("Target: ");
+  Serial.println(targetCount);
+
+  while (AVERAGE(lCount, rCount) < targetCount) {
+    lCount = ABS(encoder.getTicks(LEFT));
+    rCount = ABS(encoder.getTicks(RIGHT));
+
+    Serial.print(lCount);
+    Serial.print("\t");
+    Serial.println(rCount);
+  }
+  motors.brake();
+  delay(200);
+}
+
+// drive straight a certain distance in cm
+void driveStraight(float distance, int motorPower) {
+  delay(100);
+  marker.write(28);
+  long lCount = 0;
+  long rCount = 0;
+  long targetCount;
+  float numRev;
+  double overshoot = 2.54;  // approximate overshoot when driving a specific distance at 100 motorPower
+
+  // variables for tracking the left and right encoder counts
+  long prevlCount = 0, prevrCount = 0;
+
+  long lDiff, rDiff;  // diff between current encoder count and previous count
+
+  // variables for setting left and right motor power
+  int leftPower = motorPower;
+  int rightPower = motorPower;
+
+  // variable used to offset motor power on right vs left to keep straight.
+  int offset = 5;  // offset amount to compensate Right vs. Left drive
+  double error = 0;
+
+  numRev = (distance - overshoot) / wheelCirc;  // calculate the target # of rotations, subtracting overshoot from distance
+  targetCount = numRev * countsPerRev;          // calculate the target count
+
+  // debug
+  Serial.print("driveStraight() ");
+  Serial.print(distance);
+  Serial.print(" inches at ");
+  Serial.print(motorPower);
+  Serial.println(" power.");
+
+  Serial.print("Target: ");
+  Serial.print(numRev, 3);
+  Serial.println(" revolutions.");
+  Serial.println();
+
+  // print out header
+  Serial.print("Left\t");   // "Left" and tab
+  Serial.print("Right\t");  // "Right" and tab
+  Serial.print("Target count\t\t");
+  Serial.println("Error");
+  Serial.println("========================================");
+
+  int startTime = millis();
+  int time = 0;
+
+  encoder.clearEnc(BOTH);  // clear the encoder count
+  delay(200);              // short delay before starting the motors.
+
+  motors.drive(motorPower);  // start motors
+
+  while (AVERAGE(rCount, lCount) < targetCount) {
+    lCount = encoder.getTicks(LEFT);
+    rCount = encoder.getTicks(RIGHT);
+
+    if (time >= 50) {
+      time = 0;
+      startTime = millis();
+
+      // while the encoders read less than the target count -- debug print
+      // the encoder values and wait -- this is a holding loop.
+
+      /*Serial.print(lCount);
+      Serial.print("\t");
+      Serial.print(rCount);
+      Serial.print("\t");
+      Serial.print(targetCount);
+      Serial.print("\t");*/
+
+      motors.leftDrive(leftPower);
+      motors.rightDrive(rightPower);
+
+      // calculate the rotation "speed" as a difference in the count from previous cycle.
+      lDiff = (lCount - prevlCount);
+      rDiff = (rCount - prevrCount);
+      error = (double)ABS((lDiff - rDiff)) / 5.0;
+
+      //Serial.println(error);
+
+      // store the current count as the "previous" count for the next cycle.
+      prevlCount = lCount;
+      prevrCount = rCount;
+
+      // if left is faster than the right, slow down the left / speed up right
+      if (lDiff > rDiff) {
+        leftPower -= (int)(offset * error);
+        rightPower += (int)(offset * error);
+      }
+      // if right is faster than the left, speed up the left / slow down right
+      else if (lDiff < rDiff) {
+        leftPower += (int)(offset * error);
+        rightPower -= (int)(offset * error);
+      }
+    }
+    time = millis() - startTime;
+  }
+  // now apply "brakes" to stop the motors.
+  motors.brake();
+}
